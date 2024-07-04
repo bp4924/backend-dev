348b58180015a6c2cb40612d5720e9a474a2e70f
Bernie Petters <bp4924@gmail.com>
2024-07-04 18:17:02 -0400

    add new endpoints

--- boilerplate-project-timestamp/index.js
+++ boilerplate-project-timestamp/index.js
@@ -2,15 +2,15 @@
 // where your node app starts
 
 // init project
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require("cors");
+const cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -25,6 +25,17 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
+/* FROM GITHUB/yacob-harkati
+app.get("/api/:date?", function (req, res) {
+  let calcDate = isNaN(req.params.date) ? req.params.date : parseInt(req.params.date)
+  console.log(calcDate)
+  let currentDate = new Date(calcDate)
+  let errorResponse = {"error": `${currentDate}`}
+  let validResponse = {"unix": currentDate.getTime(), "utc": `${currentDate.toUTCString()}`}
+  console.log(currentDate)
+  currentDate == "Invalid Date" ? res.json(errorResponse) : res.json(validResponse); 
+});
+*/
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, () => {
